7d9a6ef3436920bd9efa7f9766aeae18402ed841
ivarsarner <ivar.sarner@appliedtechnology.se>
2020-05-09 17:06:40 +0200

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import StatesContainer from './components/StatesContainer';
 
 const App = () => {
   return (
-    <div className="App">
-			böb
-    </div>
+		<StatesContainer />
   );
 }
 
---
+++ src/components/State.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export interface Props {
+	stateData: {
+		state: string,
+		hospitalizedCurrently: number,
+	}
+}
+ 
+const State: React.FC<Props> = ({stateData}) => {
+	const {state, hospitalizedCurrently} = stateData;
+	
+	return (
+		<div>
+			<span>
+				{state} - {hospitalizedCurrently}
+			</span>
+			</div>
+	);
+}
+ 
+export default State;
